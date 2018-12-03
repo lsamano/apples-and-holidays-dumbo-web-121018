8322cfaadfb202a9da694b1d850da367b235cc11
Leizl Samano <leizl.samano@gmail.com>
2018-12-03 04:54:40 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,10 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_holidays = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      if supplies.include?("BBQ")
-        bbq_holidays << holiday
+      supplies.each do |supply|
+        if supply == "BBQ"
+          bbq_holidays << holiday
+        end
       end
     end
   end
